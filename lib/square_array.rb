c49932109ad6a112d4466bb0431c23383407e110
Beksultan <beksultankaiypov@gmail.com>
2020-10-08 18:35:54 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,8 +2,7 @@ def square_array(array)
  new_array = []
  counter = 0 
  while counter[counter] do 
-   array * array 
-   new_array << array 
+   new_array << array * array 
    counter =+ 1
  end
  new_array
