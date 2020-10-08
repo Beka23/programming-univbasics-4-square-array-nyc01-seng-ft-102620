65e87cdc056101fd9f805472640e42e64f0e748e
Beksultan <beksultankaiypov@gmail.com>
2020-10-08 18:57:39 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,16 +1,4 @@
 def square_array(array)
-new_array = []
- array.length.times do |index|
-   index*index
-   new_array << index
- end
-new_array
-end
-
-
-
-
-def square_array(array)
   new_array = []
   counter = 0 
   while counter < array.length
